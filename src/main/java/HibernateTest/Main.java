ce88b8f8fd7756920e7b30c035d7af656c12b92a
Роман Пантелеев <panteleevrmn95@gmail.com>
2017-05-24 17:06:04 +0300

    Spring + Hibernate throws beans

--- src/main/java/HibernateTest/Main.java
+++ src/main/java/HibernateTest/Main.java
@@ -1,10 +1,15 @@
 package HibernateTest;
 
+
+import dao.PersonDao;
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.transform.Transformers;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 import javax.imageio.spi.ServiceRegistry;
@@ -12,17 +17,34 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = sessionFactory.openSession();
+//        SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+//        Session session = sessionFactory.openSession();
+//
+//        List<Person> persons = (List<Person>)session.createQuery("from Person").list();
+//
+////        List<Person> persons1 = (List<Person>)session.createSQLQuery("select * from person").addScalar("id", Hibernate.INTEGER)
+////                .addScalar("name").addScalar("surname")
+////                .setResultTransformer(Transformers.aliasToBean(Person.class)).list();
+//
+//        for (Person p : persons)
+//            System.out.println(p);
 
-        List<Person> persons = (List<Person>)session.createQuery("from person");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("springConfig.xml");
+        PersonDao personDao = context.getBean(PersonDao.class);
 
-//        List<Person> persons1 = (List<Person>)session.createSQLQuery("select * from person").addScalar("id", Hibernate.INTEGER)
-//                .addScalar("name").addScalar("surname")
-//                .setResultTransformer(Transformers.aliasToBean(Person.class)).list();
+        Person person = new Person();
+        person.setId(2);
+        person.setName("Alexey");
+        person.setSurname("Berezin");
+        personDao.save(person);
 
-        for (Person p : persons)
+        List<Person> list = personDao.list();
+
+        for (Person p : list)
             System.out.println(p);
+
+        context.close();
+
     }
 
 }
